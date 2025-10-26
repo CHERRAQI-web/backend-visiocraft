6d3c53eb4a51e936046bbab96f04d33eb3a26ee8
CHERRAQI <aziza.cherraqi@gmail.com>
2025-10-27 00:23:12 +0100

    deployer

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -38,9 +38,9 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+secure: true,
       maxAge: 30 * 24 * 60 * 60 * 1000,
-      sameSite: 'Lax'
+      sameSite: 'none',
     });
 
     res.status(200).json({
