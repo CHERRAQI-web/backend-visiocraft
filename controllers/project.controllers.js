6c8c817cfce5efbf8ee996d6ab2687fa6cf88f32
CHERRAQI <aziza.cherraqi@gmail.com>
2025-10-26 20:31:29 +0100

    change

--- controllers/project.controllers.js
+++ controllers/project.controllers.js
@@ -1,6 +1,7 @@
 import Project from '../models/project.models.js';
 import Freelancer from '../models/freelancer.models.js';
-import Skill from '../models/skill.models.js'; 
+import Skill from '../models/skill.models.js';
+
 import AiLog from '../models/aiLog.models.js'; // Don't forget to import AiLog
 import mongoose from 'mongoose';
 import ProjetFile from '../models/project_files.models.js';
